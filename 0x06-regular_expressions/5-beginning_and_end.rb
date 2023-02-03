86c1064c1494c4c60b15b85888209abbe84e614b
Nazasophie <chinazasundaygoodness@gmail.com>
2023-02-02 18:50:05 -0800

    task5

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,3 @@
- Ruby script that creates a regular expression matching a string that starts with h ends with n and can have any single character in between
- puts ARGV[0].scan(/h.n/).join
+#!/usr/bin/env ruby
+# A Ruby script that creates a regular expression matching a string that starts with h ends with n and can have any single character in between
+puts ARGV[0].scan(/h.n/).join
